9905f6e54b6ef6742bdeda843bab20b2c169a89b
Matan Weiss <matan1111@gmail.com>
2023-01-03 18:06:03 +0200

    added methods 2, 3

--- Ex2_1.java
+++ Ex2_1.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.Random;
@@ -12,15 +14,76 @@ public class Ex2_1 {
         for (int i = 0; i < n; i++) {
             int x = rand.nextInt(bound);
             try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter("file_" + String.valueOf(i + 1), true));
+                String fileName = "file_" + String.valueOf(i + 1);
+                BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
                 for (int j = 0; j < x; j++) {
                     writer.append(s);
                 }
+                result[i] = fileName;
                 writer.close();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-            result[i] = String.valueOf(x);
+        }
+        return result;
+    }
+
+    public static int countLines(String fileName) {
+        int result = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            while (reader.readLine() != null)
+                result++;
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public static int getNumOfLines(String[] fileNames) {
+        int result = 0;
+        for (String fileName : fileNames) {
+            result += countLines(fileName);
+        }
+        return result;
+    }
+
+    public static class NumLinesThread extends Thread {
+        private String fileName;
+        private int lines;
+
+        public NumLinesThread(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public void run() {
+            lines = countLines(fileName);
+        }
+
+        public int getLines() {
+            return lines;
+        }
+    }
+
+    public static int getNumOfLinesThreads(String[] fileNames) {
+        int result = 0;
+        NumLinesThread[] threads = new NumLinesThread[fileNames.length];
+        for (int i = 0; i < fileNames.length; i++) {
+            threads[i] = new NumLinesThread(fileNames[i]);
+            threads[i].start();
+        }
+        for (int i = 0; i < fileNames.length; i++) {
+            threads[i] = new NumLinesThread(fileNames[i]);
+            threads[i].start();
+        }
+        for (int i = 0; i < fileNames.length; i++) {
+            try {
+                threads[i].join();
+                result += threads[i].getLines();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         return result;
     }
@@ -28,5 +91,7 @@ public class Ex2_1 {
     public static void main(String[] args) {
         String[] a = createTextFiles(3, 1, 10);
         System.out.println(Arrays.toString(a));
+        System.out.println(getNumOfLines(a));
+        System.out.println(getNumOfLinesThreads(a));
     }
 }
